5be47092fe5c7c2acf783efe105ada6678dd2210
Alexandre_Sarrazin <klexboss@gmail.com>
2017-01-13 14:55:24 +0100

    symlinks

---
+++ install.sh
@@ -0,0 +1,7 @@
+cd ..
+ln -s ~/config_klex/bash_aliases .bash_aliases
+ln -s ~/config_klex/bashrc .bashrc
+ln -s ~/config_klex/git-prompt.sh .git-prompt.sh
+ln -s ~/config_klex/tmux.conf .tmux.conf
+ln -s ~/config_klex/vim .vim
+ln -s ~/config_klex/vimrc .vimrc
